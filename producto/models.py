702d78d5e4113f75f535d485b0951697a7228eb2
YumiBunny <yumiimiau@gmail.com>
2017-06-15 09:26:36 -0300

    Lista de Productos, nos falta html

--- producto/models.py
+++ producto/models.py
@@ -5,19 +5,12 @@ from django.db import models
 from django.utils import timezone
 
 class Producto(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
+    nombre_de_producto = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    vencimiento = models.DateTimeField(
             blank=True, null=True)
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
     def __str__(self):
-        return self.title
+        return self.nombre_de_producto
 
 # Create your models here.
--- producto/views.py
+++ producto/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+    def producto_list(request):
+        return render(request, 'blog/producto_list.html', {})
+
 # Create your views here.

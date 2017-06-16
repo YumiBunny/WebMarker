64942724fe3a53dca8ecefac001c70b16b68271a
YumiBunny <yumiimiau@gmail.com>
2017-06-16 09:22:35 -0300

    pagina

--- producto/views.py
+++ producto/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-    def producto_list(request):
-        return render(request, 'blog/producto_list.html', {})
+def producto_list(request):
+    return render(request, 'producto/producto_list.html', {})
 
 # Create your views here.

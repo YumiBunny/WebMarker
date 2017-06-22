a0dae50859603236cb153160446f83b8be1f217f
YumiBunny <yumiimiau@gmail.com>
2017-06-22 09:20:40 -0300

    migrate

--- producto/models.py
+++ producto/models.py
@@ -6,11 +6,24 @@ from django.utils import timezone
 
 class Producto(models.Model):
     nombre_de_producto = models.CharField(max_length=200)
-    descripcion = models.TextField()
+    categoria = models.ForeignKey('Categorias')
+    precio = models.IntegerField()
+    stock = model.IntegerField()
     vencimiento = models.DateTimeField(
             blank=True, null=True)
 
     def __str__(self):
         return self.nombre_de_producto
+
+class Compra(models.Model):
+    cantidad_de_producto = models.IntegerField()
+    precio_total = models.IntegerField()
+
+class Categorias(models.Model):
+    categoria = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.categoria
+
 
 # Create your models here.

7ec708c1dae8acde7575765fa278f52f66dd51ee
Kevinmaidana <Maidanakevin35@gmail.com>
2017-07-07 11:16:06 -0300

    Pag

--- producto/admin.py
+++ producto/admin.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Producto
 from .models import Categoria
+from .models import Compra
 
 admin.site.register(Producto)
 admin.site.register(Categoria)
+admin.site.register(Compra)
 
 
 # Register your models here.

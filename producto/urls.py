dd3886ac66013506c54d41dc63e4f7572ebcdc0e
YumiBunny <yumiimiau@gmail.com>
2017-07-14 09:09:32 -0300

    mostrar datos

--- producto/urls.py
+++ producto/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.producto_list, name="views.home"),
     url(r'^contacto$', views.contacto, name="producto.views.contacto"),
     url(r'^productos$', views.productos, name="views.productos"),
-    
+    url(r'^datos/(?P<pk>[0-9]+)/$', views.datos, name="views.datos"),
 ]
--- producto/views.py
+++ producto/views.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 
 
 def producto_list(request):
     productovar = Producto.objects.all()
-    return render(request, 'producto/pag.html', {'producto': productovar})
+    return render(request, 'producto/pag.html', {'productos': productovar})
 
 def contacto(request):
     return render(request, 'producto/contacto.html', {})
 
 def productos(request):
     productovar = Producto.objects.all()
-    return render(request, 'producto/productos.html', {'producto': productovar})
+    return render(request, 'producto/productos.html', {'productos': productovar})
 
+def datos(request,pk):
+    productovar = get_object_or_404(Producto, pk=pk)
+    return render(request, 'producto/datos.html', {'producto': productovar})
 
